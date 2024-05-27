523b30a7e2150b2087ed10f84d042077fcb72914
CaptainAAI <anandaagungismail@gmail.com>
2024-05-27 10:39:41 +0700

    class AbstaksiKlas and its method

--- abstraksi/abstraksi.cpp
+++ abstraksi/abstraksi.cpp
@@ -1,2 +1,17 @@
 #include<iostream>
 using namespace std;
+
+class AbstaksiKlas {
+private: string 
+public:
+    void setXY(string a, string b) {
+        x = a;
+        x = b;
+    }
+
+    void display() {
+        cout << "x,= " << x << endl;
+        cout << "y = " << y << endl;
+    }
+};
+
