19f9f971288538eed3a5ab4e9ed09bc4c3b794ec
CaptainAAI <anandaagungismail@gmail.com>
2024-05-27 10:27:15 +0700

    fix typo

---
+++ test/test.cpp
@@ -0,0 +1,31 @@
+using namespace std;
+#include <iostream>
+
+class remotelampu {
+private:
+    string saklarNo[10];
+public:
+    void setSaklarNo(int i, string value) {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i) {
+        return saklarNo[i];
+    }
+};
+
+int main() {
+    remotelampu LampuRumah;
+
+    LampuRumah.setSaklarNo(0, "lampu Teras Rumah");
+    LampuRumah.setSaklarNo(1, "Lampu Ruang Tidur");
+    LampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    LampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+    cout << LampuRumah.getSaklarNo(0) << endl;
+    cout << LampuRumah.getSaklarNo(1) << endl;
+    cout << LampuRumah.getSaklarNo(2) << endl;
+    cout << LampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
+
+}
